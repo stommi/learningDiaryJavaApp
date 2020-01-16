bd3f0c7898a8d4cf21177027f7b8f79ef0abd2ea
TommiS <tommi.supi@gmail.com>
2020-01-16 16:15:48 +0200

    lisatty luokat aihelista ja kirjoituskone

---
+++ src/domain/Kirjoituskone.java
@@ -0,0 +1,20 @@
+package domain;
+
+import java.io.FileWriter;
+
+public class Kirjoituskone {
+
+    public Kirjoituskone() {
+
+    }
+
+    public void kirjoita(Topic teksti) {
+        try {
+            FileWriter kirjoittaja = new FileWriter("paivakirja.txt");
+            kirjoittaja.write(String.valueOf(teksti));
+            kirjoittaja.close();
+        } catch (Exception e) {
+            System.err.println("Tapahtui virhe " + e.getMessage());
+        }
+    }
+}
