09ccaa7e826f6cf49d6b167b6984495128e74af6
TommiS <tommi.supi@gmail.com>
2020-01-19 11:27:57 +0200

    lisatty tekstikayttolittyma

--- src/domain/ui/Main.java
+++ src/domain/ui/Main.java
@@ -7,36 +7,57 @@ import domain.Topic;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    Scanner lukija = new Scanner(System.in);
+    Kirjoituskone kirjoituskone = new Kirjoituskone();
+    Lista kaikkiAiheet = new Lista();
+
+    private static final String menuteksti = "\n Anna vaihtoehto:\n"
+            + "1 Lisää uusi aihe\n"
+            + "2 Tulosta aiheet\n"
+            + "3 Lopeta";
+
+    public void run() {
         Scanner lukija = new Scanner(System.in);
-        Kirjoituskone kirjoituskone = new Kirjoituskone();
-        Lista kaikkiAiheet = new Lista();
-
-        while (true) {
-            System.out.print("Syötä opiskellun aiheen otsikko: ");
-            String aihe = lukija.nextLine();
-            if (aihe.equals("")) {
+        for (; ; ) {
+            System.out.println(menuteksti);
+            String vastaus = lukija.nextLine().trim();
+            if ("1".equals(vastaus)) {
+                lisaaAihe(lukija);
+            } else if ("2".equals(vastaus)) {
+                kaikkiAiheet.tulostaAiheet();
+            } else if ("3".equals(vastaus)) {
                 break;
+            } else {
+                System.err.println(String.format("Tuntematon vaihtoehto: '%s'", vastaus));
             }
-
-            Topic t1 = new Topic(aihe);
-
-            System.out.print("Syötä kuvaus: ");
-            String kuvaus = lukija.nextLine();
-            t1.setDescription(kuvaus);
-
-            System.out.print("Syötä mahdollinen lähde esim. web url tai kirja: ");
-            String lahde = lukija.nextLine();
-            t1.setAdditionalSource(lahde);
-
-            System.out.print("Onko aiheen opiskelu kesken, syötä true tai false: ");
-            boolean tila = Boolean.parseBoolean(lukija.nextLine());
-            t1.setComplete(tila);
-
-            kirjoituskone.kirjoita(t1);
         }
-
-        kaikkiAiheet.tulostaAiheet();
-
     }
+
+
+    public static void main(String[] args) {
+        new Main().run();
+    }
+
+    private void lisaaAihe(Scanner lukija) {
+        System.out.print("Syötä opiskellun aiheen otsikko: ");
+        String aihe = lukija.nextLine();
+
+        Topic t1 = new Topic(aihe);
+
+        System.out.print("Syötä kuvaus: ");
+        String kuvaus = lukija.nextLine();
+        t1.setDescription(kuvaus);
+
+        System.out.print("Syötä mahdollinen lähde esim. web url tai kirja: ");
+        String lahde = lukija.nextLine();
+        t1.setAdditionalSource(lahde);
+
+        System.out.print("Onko aiheen opiskelu kesken, syötä true tai false: ");
+        boolean tila = Boolean.parseBoolean(lukija.nextLine());
+        t1.setComplete(tila);
+
+        kirjoituskone.kirjoita(t1);
+    }
+
 }
+
